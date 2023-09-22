b14926ae9251e36177f5c68d13fb813b037262cf
satriayudhia <satria.yudhiap@gmail.com>
2023-09-22 15:35:50 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,426 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2a---halaman-depan",
+      "name": "2A - HALAMAN DEPAN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.0944311623955434,
+        "pitch": 0.0963921025973633,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4501501862524782,
+          "pitch": 0.010545695956684753,
+          "rotation": 0,
+          "target": "2-2b---living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1---tangga",
+      "name": "1 - TANGGA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.9296459831713833,
+        "pitch": -0.38469040370041263,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2161482203013918,
+          "pitch": 0.8564264291964161,
+          "rotation": 0,
+          "target": "2-2b---living-room"
+        },
+        {
+          "yaw": 1.8449917735346961,
+          "pitch": -0.3354985487767159,
+          "rotation": 0,
+          "target": "5-3a---lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2b---living-room",
+      "name": "2B - LIVING ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.2260043609648976,
+        "pitch": 0.12907321703872654,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8404582295150291,
+          "pitch": 0.15433667051783573,
+          "rotation": 0,
+          "target": "0-2a---halaman-depan"
+        },
+        {
+          "yaw": -0.3397600751182477,
+          "pitch": 0.28618481627725423,
+          "rotation": 0,
+          "target": "4-2d---wc-1"
+        },
+        {
+          "yaw": 0.45594488050638304,
+          "pitch": 0.20213535444440822,
+          "rotation": 0,
+          "target": "3-2c---halaman-belakang"
+        },
+        {
+          "yaw": 2.875389349082866,
+          "pitch": -0.037600501127359465,
+          "rotation": 5.497787143782138,
+          "target": "1-1---tangga"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2c---halaman-belakang",
+      "name": "2C - HALAMAN BELAKANG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.7402042754734683,
+        "pitch": 0.23388345022037527,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8612428581557836,
+          "pitch": 0.2259600765117611,
+          "rotation": 0,
+          "target": "2-2b---living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-2d---wc-1",
+      "name": "2D - WC 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.392217205038918,
+        "pitch": 0.7687986847231851,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21613752106467388,
+          "pitch": 0.49121170247763857,
+          "rotation": 0,
+          "target": "2-2b---living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-3a---lantai-2",
+      "name": "3A - LANTAI 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.0093885688088893,
+        "pitch": 0.2049582836143813,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.190942541651614,
+          "pitch": 0.41102330849918367,
+          "rotation": 2.356194490192345,
+          "target": "1-1---tangga"
+        },
+        {
+          "yaw": -2.566017202050711,
+          "pitch": 0.3875844035929674,
+          "rotation": 0,
+          "target": "8-3d---wc-2"
+        },
+        {
+          "yaw": 1.9791187697632804,
+          "pitch": 0.20504583326640358,
+          "rotation": 0,
+          "target": "6-3b---bedroom-1"
+        },
+        {
+          "yaw": 0.23795706498672686,
+          "pitch": 0.07424570861300062,
+          "rotation": 0,
+          "target": "7-3c---bedroom-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-3b---bedroom-1",
+      "name": "3B - BEDROOM 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.16722829765801,
+        "pitch": 0.27030677551010385,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4033443117866415,
+          "pitch": 0.10299306165900468,
+          "rotation": 4.71238898038469,
+          "target": "5-3a---lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-3c---bedroom-2",
+      "name": "3C - BEDROOM 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.09696273622190787,
+        "pitch": 0.19917173967156998,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.549901307916743,
+          "pitch": 0.15727915683153704,
+          "rotation": 0,
+          "target": "5-3a---lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-3d---wc-2",
+      "name": "3D - WC 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8834423414389576,
+        "pitch": 0.8483678112019497,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8849440162121205,
+          "pitch": 0.33515690350517424,
+          "rotation": 0,
+          "target": "5-3a---lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-4---kantor-pemasaran",
+      "name": "4 - KANTOR PEMASARAN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GREENLAKE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
